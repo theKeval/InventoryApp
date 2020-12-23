7e4c2a97deeec429948bb7dc8efca64f245c7f42
Keval <keval.langalia@live.com>
2020-12-23 17:02:12 +0530

    adding product in UI dynamically - blueprint done

--- app/src/main/java/com/example/inventoryapp/screens/ItemListingFragment.kt
+++ app/src/main/java/com/example/inventoryapp/screens/ItemListingFragment.kt
@@ -1,17 +1,26 @@
 package com.example.inventoryapp.screens
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.children
+import androidx.core.view.get
+import androidx.core.widget.TextViewCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.inventoryapp.HomeActivity
 import com.example.inventoryapp.R
 import com.example.inventoryapp.databinding.FragmentInstructionsBinding
 import com.example.inventoryapp.databinding.FragmentItemListingBinding
+import com.example.inventoryapp.models.ProductModel
 import com.example.inventoryapp.viewmodels.HomeViewModel
 import com.example.inventoryapp.viewmodels.ItemListingViewModel
 
@@ -31,7 +40,46 @@ class ItemListingFragment: Fragment() {
         viewModel = ViewModelProvider(this).get(ItemListingViewModel::class.java)
         homeViewModel = activityViewModels<HomeViewModel>().value
 
+        homeViewModel.products.observe(viewLifecycleOwner, Observer {
+            // updateVisibilityUI(it.isEmpty())
+        })
+
+        addProduct(ProductModel("My Product", "", "", ""))
+        addProduct(ProductModel("my second product","", "", ""))
+        addProduct(ProductModel("my third product","", "", ""))
+        addProduct(ProductModel("my fourth product","", "", ""))
 
         return binding.root
+    }
+
+    fun addProduct(product: ProductModel) {
+        val linearLayout = LinearLayout(ContextThemeWrapper(context, R.style.linearLayout_products_style))
+        linearLayout.orientation = LinearLayout.VERTICAL
+
+        val textView = TextView(context)
+        textView.text = "Product Name: ${product.itemName.value}"
+        TextViewCompat.setTextAppearance(textView, R.style.label_style_productListing)
+
+        linearLayout.addView(textView)
+        binding.itemsLayout.addView(linearLayout)
+
+//        val template_view: ViewGroup? = activity?.findViewById(R.id.product_container)
+//        if(template_view != null){
+//            val productName: View? = template_view.children.find { it.id == R.id.value_product_name }
+//            (productName as TextView)?.text = product.itemName.value
+//
+//            binding.itemsLayout.addView(template_view)
+//        }
+    }
+
+    private fun updateVisibilityUI(noProducts: Boolean) {
+        if(noProducts) {
+            binding.labelNoItem.visibility = View.VISIBLE
+            binding.itemsScrollview.visibility = View.GONE
+        }
+        else {
+            binding.labelNoItem.visibility = View.GONE
+            binding.itemsScrollview.visibility = View.VISIBLE
+        }
     }
 }
